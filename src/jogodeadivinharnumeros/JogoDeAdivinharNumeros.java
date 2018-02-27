53326917371d263d04a6d16ac945d87a9f2777b2
Guilherme Rodrigues <guiigrodrigues@gmail.com>
2018-02-27 03:05:22 -0300

    Update JogoDeAdivinharNumeros.java

--- src/jogodeadivinharnumeros/JogoDeAdivinharNumeros.java
+++ src/jogodeadivinharnumeros/JogoDeAdivinharNumeros.java
@@ -23,20 +23,20 @@ public class JogoDeAdivinharNumeros {
             if(palpite < numero){
                     System.out.println("O meu número é maior. Tente de novo");
             } else {
-                System.out.println("Parabens voce acertou meu numero em: " + tentativas + " tentativa(s)!");
+                System.out.println("Parabéns voce acertou meu numero em: " + tentativas + " tentativa(s)!");
                  if(tentativas == 1 ){
                      System.out.println("Que Sorte !!");
-                 } if (tentativas < 11 && tentativas > 1){
+                 }  if (tentativas < 11 && tentativas > 1){
                      System.out.println("Muito Bem !!");
-                 } if (tentativas > 10) {
+                 }  if (tentativas > 10) {
                      System.out.println(" ");
-                 } 
-                }
+                    } 
+              }
                     if (tentativas > 20) {
                     System.out.println("Infelizmente você perdeu! Meu número foi " + numero);
-                }
-            }
-        }
+                    }
+          }
+    }
             
     public static void Jogar(){
         int palpite=0, 
@@ -51,15 +51,16 @@ public class JogoDeAdivinharNumeros {
         
         Random geradorDeAleatorios = new Random();
         sorteado = geradorDeAleatorios.nextInt(250) + 1;
-        System.out.println("Adivinhe o número!");
         System.out.println("Número entre 1 e 250 sorteado!");
         
+        // laço para o jogo rodar pelo menos uma unica vez
         do{
             System.out.printf("\n\n-----------------\n");
             System.out.println("Número de tentativas: " + tentativas + " de 20");
             
             System.out.print("Qual seu palpite: ");
             
+            //laço para o validador de entrada
             do{
             try{
                palpite = Integer.parseInt(entrada.nextLine());
@@ -68,33 +69,43 @@ public class JogoDeAdivinharNumeros {
                 System.out.println(" ");
                 System.out.println("Por favor, digite apenas números!");
                 System.out.println(" ");
-                flag = false; // para ter certeza 
+                flag = false; // para ter certeza
             } 
-            }while(!flag);   
+            }while(!flag); //enquanto flag for false roda o validador  
             
             
             tentativas++;
-            
             dica(palpite,sorteado, tentativas);
+            
         }while (palpite != sorteado && tentativas != 21);
         System.out.println(" ");
         
+        //laço para jogar novamente
         do{
         System.out.println("Deseja jogar novamente ?");
         System.out.println("1- Sim ou 2-Nao");
-         playAgain = entrada.nextInt();
-            if (playAgain != 1 && playAgain !=2) {
-                System.out.println("Entrada errada, por favor faça uma entrada valida");
-            }
-            System.out.println("");
+        
+         //condição para caso o usuario digitar um comando invalido
+            do{
+                try{
+                    playAgain = Integer.parseInt(entrada.nextLine());
+                    flag = true;
+                } catch(NumberFormatException e){
+                    System.out.println(" ");
+                    System.out.println("Por favor, digite apenas números!");
+                    System.out.println(" ");
+                    flag = false; 
+                  } 
+            }   while(!flag); //enquanto flag for false roda o validador 
+            
         } while(playAgain != 1 && playAgain !=2);
         
-        if (playAgain == 1) {
-            Jogar();
-        } else {
-            System.out.println("Obrigado por jogar.");
-            System.exit(0);
-        }
+            if (playAgain == 1) {
+                Jogar();
+            } else {
+                System.out.println("Obrigado por jogar.");
+                System.exit(0);
+              }
     }
     
     public static void main(String[] args) {
